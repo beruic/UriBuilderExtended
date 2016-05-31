51cb045952ab6c127af5ec087a92011345cbaa70
Jimmy Merrild Krag <jimmy.krag@d4tec.com>
2016-05-31 13:27:45 +0200

    Added App.config and updated AssemblyInfo

--- UriBuilderExtended/Properties/AssemblyInfo.cs
+++ UriBuilderExtended/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("UriBuilderExtended")]
-[assembly: AssemblyDescription("Extensions with features missing from .NET's System.UriBuilder.")]
+[assembly: AssemblyDescription("Extensions to .NET's System.UriBuilder.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("UriBuilderExtended")]
